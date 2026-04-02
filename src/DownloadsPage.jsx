7814af3a7914ae7bf987d30838942a13e548f073
yuri koshkin <yuri.koshkin@gmail.com>
2026-04-02 18:59:34 +0300

    downloads update

--- src/DownloadsPage.jsx
+++ src/DownloadsPage.jsx
@@ -9,10 +9,10 @@ export default function DownloadsPage() {
         </a>
         <h1>Downloads</h1>
         <p>
-          <a href="/files/Knob16-surface-control.zip" download>
-            Knob16-surface-control.zip
+          <a href="/files/Remote%20Scripts.zip" download>
+            Remote Scripts.zip
           </a>{' '}
-          — 2.4 KB — Modified March 31, 2026
+          — 12.3 KB — Modified April 2, 2026
         </p>
       </div>
     </div>
